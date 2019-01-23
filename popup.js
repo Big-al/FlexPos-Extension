1cbb4cc7bf85c3de505200abc05be55b402055be
Alanmk <a@amk.io>
2019-01-23 23:24:49 +0100

    Initial version

---
+++ popup.js
@@ -0,0 +1,8 @@
+// ===================================================== > Runs at page load < ===========================================================
+document.addEventListener('DOMContentLoaded', function () {
+}, false);
+
+// ===================================================== > Button Functions < ============================================================
+function goToWebsite() {
+    window.location.href = "http://www.login.flexpos.com";
+}
